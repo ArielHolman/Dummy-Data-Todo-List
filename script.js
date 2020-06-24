b476d0362369efa199467dd49c9271fc07b0aa98
ArielHolman <ArielARodrigues01@gmail.com>
2020-06-24 16:45:35 -0500

    got color to work for true and false

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ let arrayOfTodos = [
     "completed": false
   }
 ]
+let fontColor =''
 
 const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
@@ -24,23 +25,35 @@ const logTodos = () => {
 }
 
 const populateTodos = () => {
-    let unicorn = document.getElementById("todo-list")
-    let poop = document.createElement('LI')
-    let fork =document.createTextNode(arrayOfTodos[0].title)
+    let list = document.getElementById("todo-list")
 
-    poop.appendChild(fork) 
-    unicorn.appendChild(poop)
+    for (i=0; i < arrayOfTodos.length; i++){
+    let newLi = document.createElement('LI')
+    let newH1 = document.createElement('H1')
+    let newH2 = document.createElement('H2')
+    let newH3 = document.createElement('H3')
+    let newH4 = document.createElement('H4')
+
+    let userId =document.createTextNode(arrayOfTodos[i].userId)
+    newH1.appendChild(userId)
+
+    let id =document.createTextNode(arrayOfTodos[i].id)
+    newH2.appendChild(id)
+
+    let title =document.createTextNode(arrayOfTodos[i].title)
+    newH3.appendChild(title)
+
+    let completed=document.createTextNode(arrayOfTodos[i].completed)
+    newH4.appendChild(completed)
+    newLi.appendChild(newH1)
+    newLi.appendChild(newH2)
+    newLi.appendChild(newH3)
+    newLi.appendChild(newH4)
+    list.appendChild(newLi)
+
+    if (arrayOfTodos[i].completed === true) {
+        fontColor = 'blue'} else {fontColor = 'red'}
+
+        newH4.style.color=fontColor
+    }
 }
-
-
-// console.log(arrayOfTodos[0].title) // => 14
-// console.log(arrayOfTodos[1].userId) // => 20
-
-// let list = () => {
-//     let unicorn = document.getElementById("todo-list")
-//     let poop = document.createElement('LI')
-//     let fork =poop.createTextNode(arrayOfTodos[0].title)
-
-//     poop.appendChild(fork) 
-//     unicorn.appendChild(poop)
-// }
